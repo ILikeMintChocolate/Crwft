8d1e9b2c20a93a1933774329d7bf81ac06c42f91
ILikeMintChocolate <99123542+ILikeMintChocolate@users.noreply.github.com>
2022-06-02 04:21:10 +0900

    Add files via upload

--- Client/src/App.svelte
+++ Client/src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-	import EditorPage from "./Page/EditorPage.svelte";
+    import {push} from 'svelte-spa-router'
+    import Router from 'svelte-spa-router'
+    import routes from './routes'
+    push('/login')
+    
+    //import EditorPage from "./page/EditorPage.svelte";
+    //<EditorPage></EditorPage>
 </script>
 
-<main>
-	<EditorPage></EditorPage>
-</main>
 
-<style>
-    
-</style>
+<Router {routes} /> 
