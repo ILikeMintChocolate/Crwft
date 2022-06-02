31185a07401d1de1b039107b790bf45efd347394
ILikeMintChocolate <99123542+ILikeMintChocolate@users.noreply.github.com>
2022-06-03 02:32:32 +0900

    Add files via upload

--- Client/src/routes.js
+++ Client/src/routes.js
@@ -1,12 +1,14 @@
 import LoginPage from "./page/LoginPage.svelte";
 import HomePage from "./page/HomePage.svelte";
 import EditorPage from "./page/EditorPage.svelte";
+import DemoPage from "./page/DemoPage.svelte";
 
 
 const routes = {
     "/login": LoginPage,
     "/home": HomePage,
     "/editor/:id": EditorPage,
+    "/demo/:code&:name": DemoPage,
 };
 
 export default routes
