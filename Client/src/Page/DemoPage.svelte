3a64ab8b1c1820b5d98552bf3c69adfda54cd955
ILikeMintChocolate <99123542+ILikeMintChocolate@users.noreply.github.com>
2022-06-03 02:32:52 +0900

    Add files via upload

--- Client/src/Page/DemoPage.svelte
+++ Client/src/Page/DemoPage.svelte
@@ -4,13 +4,16 @@
 
     let roomCode = splitString2[0]
     let userName = splitString2[1]
+
+    console.log(roomCode)
+    console.log(userName)
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
 <main>
 
     <!-- svelte-ignore a11y-missing-attribute -->
-    <iframe src={"http://localhost:8000/enter?code="+roomCode+"&name="+userName} style="width: 100%; height:99vh" allow="display-capture; autoplay;"></iframe>
+    <iframe src={"https://crwftwebrtc.loca.lt/enter?code="+roomCode+"&name="+userName} style="width: 100%; height:99vh" allow="display-capture; autoplay;"></iframe>
 </main>
 
 
--- Client/src/Page/HomePage.svelte
+++ Client/src/Page/HomePage.svelte
@@ -56,6 +56,7 @@
 
     async function getOrdersByUser() {
         const tempUserUID = getUserUID();
+        //const tempUserUID = 'Kolhg56pseZMgMPrq1Ou6E6O1HE2';
         const querySnapshot = await getDocs(collection($firebaseDB, tempUserUID));
 
         let tempArray1 = [];
@@ -76,27 +77,24 @@
     }, []);
 
 
+    let hoverBool = false;
 
-    
-    
-    
-
-
-
-
-
-    
-
-    function clickFile() {
-        alert('파일 불러오기')
+    function enterButtonHover() {
+        hoverBool = true;
     }
 
-    
-    
+    function enterButtonLeave() {
+        hoverBool = false;
+    }
+
+    let buttonWrapper;
+    let enterButton;
+
+    function clickCreateFileButton() {
+        push(`/editor/${fileArray.length}`)
+    }
     
 
-    
-    
 
 </script>
 
@@ -126,29 +124,90 @@
     <div id="home-section-box" style="width: {innerWidth}px;">
         <div id="home-section" style="height: {innerHeight-50}px">
         
-            <div id="page-title">My Workspace</div>
+            <div id="page-title">내 활동</div>
 
-            <button id="create-file-button" on:click={()=>{
-                push(`/editor/${fileArray.length}`)
-            }}>
-                <div class="page-icon-1">
-                    <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="3" y="11" width="6" height="1" fill="#FFFCEF"/>
-                    <rect x="3" y="9" width="6" height="1" fill="#FFFCEF"/>
-                    <rect x="3" y="7" width="6" height="1" fill="#FFFCEF"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H8V1H1V15H11V4H12V16H0V0Z" fill="#FFFCEF"/>
-                    <rect x="7" y="0.999985" width="1" height="3" fill="#FFFCEF"/>
-                    <rect x="7" y="4.99998" width="1" height="4" transform="rotate(-90 7 4.99998)" fill="#FFFCEF"/>
-                    <rect x="8.00854" y="0.0205994" width="5.63619" height="1.19861" transform="rotate(45 8.00854 0.0205994)" fill="#FFFCEF"/>
-                    </svg>
+            <div id="button-wrapper" bind:this={buttonWrapper}>
+                <button id="create-file-button" on:click={clickCreateFileButton}>
+                    <div class="page-icon-1">
+                        <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="3" y="11" width="6" height="1" fill="#FFFCEF"/>
+                            <rect x="3" y="9" width="6" height="1" fill="#FFFCEF"/>
+                            <rect x="3" y="7" width="6" height="1" fill="#FFFCEF"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H8V1H1V15H11V4H12V16H0V0Z" fill="#FFFCEF"/>
+                            <rect x="7" y="1" width="1" height="3" fill="#FFFCEF"/>
+                            <rect x="7" y="5" width="1" height="4" transform="rotate(-90 7 5)" fill="#FFFCEF"/>
+                            <rect x="8.00854" y="0.0206299" width="5.63619" height="1.19861" transform="rotate(45 8.00854 0.0206299)" fill="#FFFCEF"/>
+                        </svg>
+                    </div>
+                    <div id="button-title">
+                        새 작업
+                    </div>
+                    <div class="plus-icon">
+                        <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect y="4" width="9" height="1" fill="#FFFCEF"/>
+                            <rect x="4" y="9" width="9" height="1" transform="rotate(-90 4 9)" fill="#FFFCEF"/>
+                        </svg>
+                    </div>
+                </button>
+
+
+                <div id="enter-share-button-wrapper">
+                    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+                    <button id="enter-share-button" on:mouseover={enterButtonHover} on:mouseleave={enterButtonLeave}>
+                        {#if hoverBool==false}
+                        <div id="before-hover">
+                            <div class="center-column">                            
+                                <div id="enter-button-title" style="width: 180px;" bind:this={enterButton}>
+                                    <div>
+                                        <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 0L12 8L0 16V0Z" fill="#4DB15D"/>
+                                        </svg>
+                                    </div>
+                                    <div style="margin-top: -2px;">공유 작업 보기</div>
+                                </div>
+                            </div>
+                        </div>
+                        {:else}
+                        <div id="after-hover" style="height: 50px;">
+                            <div class="center-column"  style="height: 50px;">
+                                <div class="center-row">
+                               
+                                    <input class="input" id="roomCode" type="text" value="" required placeholder="참여코드">
+                                    <input class="input" id="userName" type="text" value="" required placeholder="이름">
+                                
+                                    <button id="enter-button" on:click={()=>{
+                                        push(`/demo/code=${document.getElementById("roomCode").value},name=${document.getElementById("userName").value}`)
+                                    }}>
+                                        <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 0L12 8L0 16V0Z" fill="#4DB15D"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        {/if}
+                    </button>
                 </div>
-                <div id="button-title">
-                    New File
-                </div>
-                <div class="plus-icon">
-                </div>
-                
-            </button>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             <div id="file-array" class="grid-container scroll-style" >
                 {#each fileArray as file}
@@ -202,6 +261,7 @@
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
 
+    
     main {
         background-color: #ffffff;
         display: flex;
@@ -274,7 +334,7 @@
 
     #button-title {
         font-family: 'Noto Sans KR', sans-serif;
-        font-weight: 700;
+        font-weight: 200;
         font-size: 18px;
         color: #ffffff;
         margin-top: 5px;
@@ -289,15 +349,13 @@
     .page-icon-2 {
         width: 12px;
         height: 16px;
-        background-image: url('../icon/pageIcon2.png');
         margin-top: 18px;
     }
 
     .plus-icon {
         width: 9px;
         height: 9px;
-        /*background-image: url('../icon/plusIcon.png');*/
-        margin-top: 14px;
+        margin-top: 6px;
     }
 
     #file-array {
@@ -384,6 +442,114 @@
     #user-thumbnail {
         width: 32px;
         height: 32px;
+    }
+
+    #button-wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 525px;
+        height: 50px;
+    }
+
+
+
+    #enter-share-button {
+        border: 1px solid #4DB15D;
+        width: 180px;
+        height: 50px;
+        border-radius: 8px;
+        display: flex;
+        justify-content: space-around;
+        flex-direction: row;
+        background-color: #ffffff;
+        margin: 0;
+        padding: 0;
+        
+    }
+
+    #enter-share-button:hover {
+        animation-name: scaleButton;
+        animation-duration: 0.5s;
+        animation-timing-function: ease-in-out;
+        animation-iteration-count: 1;
+        animation-direction: normal;
+        animation-fill-mode: forwards;
+        animation-play-state: running;
+    }
+
+    @keyframes scaleButton {
+        from {
+          width: 180px;
+          height: 50px;
+        }
+        to {
+          width: 250px;
+          height: 50px;
+        }
+    }
+
+
+    #enter-button-title {
+        font-family: 'Noto Sans KR', sans-serif;
+        font-weight: 200;
+        font-size: 18px;
+        color: #383838;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        
+    }
+
+    #enter-share-button-wrapper {
+        width: 300px;
+        height: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: left;
+    }
+    
+    #before-hover {
+        display: flex;
+        justify-content: space-around;
+        flex-direction: row;
+        width: 180px;
+        height: 50px;
+    }
+
+    #roomCode {
+        width: 80px;
+        height: 30px;
+        border: 0;
+        background-color: transparent;
+    }
+
+    
+    #userName {
+        width: 80px;
+        height: 30px;
+        border: 0;
+        background-color: transparent;
+    }
+
+    .input {
+        margin-top: 8px;
+    }
+
+    .input:focus {
+        border: 0;
+        outline: 0; 
+    }
+
+    #enter-button {
+        border: 0;
+        background-color: transparent;
+        margin-top: 8px;
+    }
+
+    #enter-button:focus {
+        background-color: transparent;
+        border: 0;
     }
 
     
